5efcd98187776da395b3b23ffe0b73d27e426366
kauacarreira-cloud <kaua.carreira@escola.pr.gov.br>
2025-10-03 13:33:26 -0300

    update

---
+++ brellyy.js
@@ -0,0 +1,36 @@
+const imagens = document.querySelectorAll("img");
+const botaoImagens = document.getElementById("Imagens");
+
+let trocou = false;
+
+
+const novasImagens = {
+    "1.jpeg": "9.jpeg",
+    "2.jpeg": "10.jpeg",
+    "3.jpeg": "11.jpeg",
+    "4.jpeg": "12.jpeg",
+    "5.jpeg": "13.jpeg",
+    "6.jpeg": "14.jpeg",
+    "77logo.jpeg": "raulogo.jpeg",
+    "77cima.jpeg": "raucima.jpeg"
+};
+
+botaoImagens.addEventListener("click", () => {
+    if (!trocou) {
+        imagens.forEach(img => {
+            img.dataset.original = img.src; 
+            const arquivo = img.src.split("/").pop(); 
+            if (novasImagens[arquivo]) {
+                img.src = novasImagens[arquivo];
+            }
+        });
+        trocou = true;
+    } else {
+        imagens.forEach(img => {
+            if (img.dataset.original) {
+                img.src = img.dataset.original; // volta ao original
+            }
+        });
+        trocou = false;
+    }
+});
